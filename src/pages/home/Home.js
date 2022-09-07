6a57eb224d691b6da1e1adf0484c42987122c61c
ucoder.git <ucoder.git@gmail.com>
2022-09-07 11:22:54 +0900

    리액트 파이어베이스 프로젝트

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+import React, { Component }  from 'react';
+
+
+export default function Home() {
+    return (
+        <div>
+            HOME
+        </div>
+    )
+}
