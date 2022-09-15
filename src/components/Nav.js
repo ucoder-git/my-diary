6c4e758ec4c4a054faffa56ce9d5532306bc2dbc
ucoder.git <ucoder.git@gmail.com>
2022-09-16 04:09:30 +0900

    로그아웃 버튼

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,18 @@
 import styles from './Nav.module.css'
 import { Link } from 'react-router-dom'
 import React, { Component }  from 'react';
-
-
+import { useLogout } from '../hooks/useLogout';
 
 export default function Nav() {
+    const { logout } = useLogout();
+    
     return (
         <nav className={styles.nav}>
             <h1 className={styles.tit}>두근 두근 비밀일기</h1>
             <ul className={styles.list_nav}>
                 <li><Link to="/login">로그인</Link></li>
                 <li><Link to="/signup">가입하기</Link></li>
+                <li><button type='button' onClick={logout}>로그아웃</button></li>
             </ul>
         </nav>
     )
