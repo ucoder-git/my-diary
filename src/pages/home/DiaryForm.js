ec8ce92098629aa5e0ebf5a4c80a4f82e26773af
ucoder-git <ucoder.git@gmail.com>
2022-09-17 16:52:38 +0900

    홈화면 구성

---
+++ src/pages/home/DiaryForm.js
@@ -0,0 +1,17 @@
+export default function DiaryForm() {
+    return (
+        <>
+        <form>
+            <fieldset>
+                <legend>일기 쓰기</legend>
+                <label htmlFor="tit"> 일기 제목: </label>
+                <input id="tit" type='text' required />
+                <label htmlFor="tit"> 일기 내용: </label>
+                <textarea id="tit" type='text' required />
+                <button type="submit">저장하기</button>
+            </fieldset>
+        </form>
+        </>
+
+    )
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,17 @@
 import styles from './Home.module.css'
 import React, { Component }  from 'react';
+import DiaryForm from './DiaryForm';
 
 
 export default function Home() {
     return (
         <main className={styles.cont}>
-        <aside className={styles.side_menu}></aside>
-
+        <aside className={styles.side_menu}>
+            <DiaryForm></DiaryForm>
+        </aside>
+        <ul className={styles.content_list}>
+            diary list
+        </ul>
         </main>
     )
 }
